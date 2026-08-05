082507fa9ff54f58f99bb0161c933d8eeb6d9407
kiel <kiel888897@gmail.com>
2026-08-05 08:48:01 +0800

    menu nav di sable

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 console.log("SJCM Website Loaded");
-AOS.init({
-  duration: 800,
-  once: true,
-});
+
+if (typeof AOS !== "undefined") {
+  AOS.init({
+    duration: 800,
+    once: true,
+  });
+}
 
 const menuButton = document.getElementById("mobile-menu-button");
-
 const mobileMenu = document.getElementById("mobile-menu");
 
-menuButton.addEventListener("click", () => {
-  mobileMenu.classList.toggle("hidden");
+if (menuButton && mobileMenu) {
+  menuButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
 
-  const icon = menuButton.querySelector("i");
-
-  icon.classList.toggle("fa-bars");
-
-  icon.classList.toggle("fa-xmark");
-});
+    const icon = menuButton.querySelector("i");
+    if (icon) {
+      icon.classList.toggle("fa-bars");
+      icon.classList.toggle("fa-xmark");
+    }
+  });
+}
